c1a8b6b175bb13b7b452ffa7a4b43fc883e1dc14
SeanX1209 <136871786@qq.com>
2020-07-13 09:57:36 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("1");
+        System.out.println("3");
+        System.out.println("5");
+        System.out.println("7");
+        System.out.println("888");
+    }
+}
